803c65cb8e0d5f202f65a935dca06cb553d65dec
HydreFunky <zamboni.benjamin@hotmail.com>
2016-10-23 14:59:10 +0200

    Optimisation SQL

--- Class/Membre.class.php
+++ Class/Membre.class.php
@@ -14,25 +14,22 @@
 		
 		public function register_member()
 		{
-			$requete = "SELECT * FROM compte WHERE email = :email";  
-			$req_prep = $connexion->prepare($requete);
-			$req_prep->execute(array(':email'=>$this->email));
-			$resultat = $req_prep->fetchAll(); 
-			$result = $resultat[0];
-			
-			if($result['email'] != $this->email)
-			{
 			include 'Class/Mysql.php';
-			$stmt = $connexion->prepare("INSERT INTO compte (username, motdepasse, email) VALUES (:username, :motdepasse, :email)");
-			$stmt->bindParam(':username', $this->username);
-			$stmt->bindParam(':motdepasse', $this->motdepasse);
-			$stmt->bindParam(':email', $this->email);
-			$stmt->execute();
-			return '<b>Inscription réussite, vous pouvez maintenant vous connectez <a href="?load=connexion">Se connecter</a></b>';
+			$requete = $sql->query('SELECT email, username FROM compte');  
+			while($result = $requete->fetch())
+			if($result['email'] == $this->email OR $result['username'] == $this->username)
+			{
+				echo 'ERREUR : Adresse E-mail ou Nom d\'utilisateur déjà utilisée ';
 			}
 			else
 			{
-				echo 'ERREUR : Adresse E-mail déjà utilisée';
+				include 'Class/Mysql.php';
+				$stmt = $sql->prepare("INSERT INTO compte (username, motdepasse, email) VALUES (:username, :motdepasse, :email)");
+				$stmt->bindParam(':username', $this->username);
+				$stmt->bindParam(':motdepasse', $this->motdepasse);
+				$stmt->bindParam(':email', $this->email);
+				$stmt->execute();
+				return '<b>Inscription réussite, vous pouvez maintenant vous connectez <a href="?load=connexion">Se connecter</a></b>';
 			}
 		}
 		
@@ -40,7 +37,7 @@
 		{
 			include 'Class/Mysql.php';
 			$requete = "SELECT * FROM compte WHERE email = :email";  
-			$req_prep = $connexion->prepare($requete);
+			$req_prep = $sql->prepare($requete);
 			$req_prep->execute(array(':email'=>$this->email));
 			$resultat = $req_prep->fetchAll(); 
 			$result = $resultat[0];
--- Class/Mysql.php
+++ Class/Mysql.php
@@ -6,12 +6,15 @@ $PARAM_utilisateur='root'; // nom d'utilisateur pour se connecter
 $PARAM_mot_passe=''; // mot de passe de l'utilisateur pour se connecter
 try
 {
-        $connexion = new PDO('mysql:host='.$PARAM_hote.';dbname='.$PARAM_nom_bd, $PARAM_utilisateur, $PARAM_mot_passe, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+        $sql = new PDO('mysql:host='.$PARAM_hote.';dbname='.$PARAM_nom_bd, $PARAM_utilisateur, $PARAM_mot_passe, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
 }
  
 catch(Exception $e)
 {
-        echo 'Erreur : '.$e->getMessage().'<br />';
-        echo 'N° : '.$e->getCode();
+	//Gestion Erreur SQL
+	if($e->getCode() == 1049)
+	{
+		die('La base de donnée est introuvable');
+	}
 }
 ?>
