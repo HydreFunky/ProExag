6cad9c9d09474197ee0085239ff63f7d7a4687c7
HydreFunky <zamboni.benjamin@hotmail.com>
2016-10-23 21:22:13 +0200

    Optimisation SQL #2

--- Class/Exercice.class.php
+++ Class/Exercice.class.php
@@ -15,7 +15,7 @@
 		public function register_exercice()
 		{
 			include 'Class/Mysql.php';
-			$stmt = $connexion->prepare("INSERT INTO resultat (username, exercice, score) VALUES (:username, :exercice, :score)");
+			$stmt = $sql->prepare("INSERT INTO resultat (username, exercice, score) VALUES (:username, :exercice, :score)");
 			$stmt->bindParam(':username', $this->username);
 			$stmt->bindParam(':exercice', $this->exercice);
 			$stmt->bindParam(':score', $this->score);
@@ -26,7 +26,7 @@
 		{
 			include 'Class/Mysql.php';
 			$requete = "SELECT * FROM resultat WHERE username = :username ORDER BY score DESC limit 0,2";  
-			$req_prep = $connexion->prepare($requete);
+			$req_prep = $sql->prepare($requete);
 			$req_prep->execute(array(':username'=>$this->username));
 			while ($donnees = $req_prep->fetch())
 			echo 'Exercice N°'.$donnees['exercice'].' Score : '.$donnees['score'].'<br>';
